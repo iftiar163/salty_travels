98855bbc0d3d37b447218cc994470db9006eefed
Iftiar Hossain <iftiar.hossain163@gmail.com>
2024-01-01 00:40:46 +0600

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+import router from "./routes/router";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
