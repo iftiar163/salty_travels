4f6d2229eaf9dfe24d36292d86b17f47577eddcf
Iftiar Hossain <iftiar.hossain163@gmail.com>
2024-01-12 00:28:20 +0600

    salt

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./Header.scss";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -7,12 +8,48 @@ const Header = () => {
       <header>
         <Container>
           <Row>
+            {/* Logo */}
             <Col md={4}>
               <div className="logo">
-                <img src="../../../src/assets/img/Logo.png" alt="" />
+                <Link to={"/"}>
+                  <img
+                    src="https://res.cloudinary.com/dwrvexxbo/image/upload/v1704995968/salty_travels/br5kmwwfgoje8bh7cksq.png"
+                    alt=""
+                  />
+                </Link>
               </div>
             </Col>
-            <Col md={8}>Vuda</Col>
+            <Col md={8}>
+              {/* Menu */}
+              <Row>
+                <Col md={8}>
+                  <div className="menu_link">
+                    <ul>
+                      <li>
+                        <Link to={"/"}>Home</Link>
+                      </li>
+                      <li>
+                        <Link to={"/about-us"}>About Us</Link>
+                      </li>
+                      <li>
+                        <Link to={"Tours"}>Destinations</Link>
+                      </li>
+                      <li>
+                        <Link to={"/blog"}>Blog</Link>
+                      </li>
+                    </ul>
+                  </div>
+                </Col>
+                {/* Book Now Button */}
+                <Col md={4}>
+                  <div className="book-now">
+                    <Link to={"/book-now"}>
+                      <Button className="book_button">Book Now</Button>
+                    </Link>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
           </Row>
         </Container>
       </header>
